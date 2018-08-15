9b039620ae1cbcaf49a86a50b52d1b4d7a8f35fd
Riazahmedsg <riazahmedsg@iconixvav.com>
2018-08-15 11:37:14 -0400

    first commit

--- public/Scripts/objects/addressbook.js
+++ public/Scripts/objects/addressbook.js
@@ -1,12 +1,30 @@
-(function(){
+/* Custom JS goes here */
+/* File name:           addressbook.js
+Author's name:          Riaz Ahmed
+web site name:          riazahmedsg.github.io
+file description:       contains Javascript code Code      */
+(function () {
     class AddressBook {
         constructor() {
             this.Contacts = [];
         }
 
         // complete the addressbook class
+        addItem(newItem) {
+            this.items.push(newItem);
+        }
+
+
     }
 
     objects.AddressBook = AddressBook;
 
 })(objects || (objects = {}));
+
+
+
+
+// let address = new AddressBook();
+// address.addItem(item1);
+// address.addItem(item2);
+// address.addItem(item3);
--- public/Scripts/objects/contact.js
+++ public/Scripts/objects/contact.js
@@ -2,6 +2,12 @@ let objects;
 (function(){
     class Contact {
         // your class definition goes here
+        constructor(firstName = "", lastName = "", contactNumber = "", emailAddress = "") {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.contactNumber = contactNumber;
+            this.emailAddress = emailAddress;
+        }
     }
 
     objects.Contact = Contact;
